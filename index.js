1c09dd1bf8e019122e2cb609591b7bdf9fee817c
msusur <mail@mertsusur.com>
2016-04-26 21:08:47 +0300

    application implementation

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-var express = require('express'), 
-	 app = express();
+var express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	router = express.Router(),
+	trello = new require("node-trello");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/', express.static('src'));
-app.post('/api', function(res, req){
+
+router.get("/tasks", function (req, res) {
+	// var t = new trello("");
+	// t.get("/1/lists/gb1NPywq", function (result) {
+	// 	if (result.statusCode === 200) {
+	// 		console.log(result);
+	// 		res.json({ message: 'hooray! welcome to our api!' });
+	// 	} else {
+	// 		res.json({ message: "Error", statusMessage: result.statusMessage, body: result.responseBody });
+	// 	}
+	// });
 
 });
 
-app.listen(8080, function(){
+app.use("/api", router);
+
+app.listen(8080, function () {
 	console.log("listening on 8080");
 });
--- src/app/main.js
+++ src/app/main.js
@@ -1,7 +1,20 @@
-(function(angular){
+(function (angular) {
     "use strict";
     angular.module("testApp", [])
-    .controller("mainController", ["$scope", function($scope){
-        $scope.title = "yay!";
-    }]);
+        .controller("mainController", ["$scope", "$http", function ($scope, $http) {
+
+            (function ($scope, $http) {
+                $http.get("/api/tasks", function (result) {
+                    debugger;
+                });
+            })($scope, $http);
+            $scope.tasks = [
+                { nr: 1, title: "Yay!" },
+                { nr: 2, title: "Yay!" },
+                { nr: 3, title: "Yay!" }
+            ];
+            $scope.comment = function (item) {
+                alert("not working yet.");
+            };
+        }]);
 })(angular);
